abcc890185a7ffd4239d39b84d291e0a63f4351f
Maksin Spizhuk <indcheat@mail.ru>
2025-04-02 10:14:40 +0300

    added arrays-two.js

---
+++ arrays-two.js
@@ -0,0 +1,16 @@
+const checkEven = (arr) => {
+  let arrLength = arr.length;
+  for (let i = 0; i < arrLength; i++) {
+    if (arr[i] % 2 !== 0) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const mainArr = [2, 4, 6, 8, 10, 12];
+const secArr = [2, 4, 6, 6, 8, 9];
+
+console.log(checkEven(mainArr));
+console.log(checkEven(secArr));
